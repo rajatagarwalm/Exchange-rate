0711b23d44b57c25fcb6d8c5551f8f5f09d7dcc8
swinstall <swinstall@IGTN-WSTR-DL001.jkcement.com>
2020-07-05 16:44:31 +0530

    Updates

---
+++ precache-manifest.5c11c863e35d930a8a689e0ea18b3fce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "da11558138a96efa6103",
+    "url": "/Exchange-rate/static/css/main.a78f7c5c.chunk.css"
+  },
+  {
+    "revision": "da11558138a96efa6103",
+    "url": "/Exchange-rate/static/js/main.da115581.chunk.js"
+  },
+  {
+    "revision": "a0cc1fcda9029297d46c",
+    "url": "/Exchange-rate/static/css/1.80ef935a.chunk.css"
+  },
+  {
+    "revision": "a0cc1fcda9029297d46c",
+    "url": "/Exchange-rate/static/js/1.a0cc1fcd.chunk.js"
+  },
+  {
+    "revision": "9445fc9027822c440864",
+    "url": "/Exchange-rate/static/js/runtime~main.9445fc90.js"
+  },
+  {
+    "revision": "9b2d878f2c0d4e939810966630c59d13",
+    "url": "/Exchange-rate/index.html"
+  }
+];
