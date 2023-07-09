5403ef3315700cc5fe7303c42f427814fecdcb04
Rajat Agarwal <rajat.agarwal@intimetec.com>
2023-07-09 12:15:10 +0530

    changes some styling

--- src/components/ExchangeRate.js
+++ src/components/ExchangeRate.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import { fetchExchangeRatesFromBaseCurrency } from '../service/apiCalls';
 import './ExchangeRate.css';
 
